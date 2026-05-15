471a9ca721a9b0603f5073d44660021cc8fcbd9d
Ben2557 <91072875+Ben2557@users.noreply.github.com>
2026-05-15 15:35:18 +0200

    Allow custom overlays apply

--- customize.sh
+++ customize.sh
@@ -133,6 +133,208 @@ smali_kit() {
   done
 }
 
+# Adds the dummy-APK safeguard used by custom themes that contain tiny placeholder
+# overlay APKs. Those files make Samsung's OverlayManager parser crash, so we
+# remove them from /data/overlays/currentstyle and from mEnabledPackages before
+# ThemeManager asks the framework to apply overlays.
+patch_dummy_overlay_apks() {
+  local decompiled="$1" tm tmp method_file
+
+  tm=$(find "$decompiled" -path "*/com/samsung/android/thememanager/ThemeManager.smali" 2>/dev/null | head -n 1)
+  [ -f "$tm" ] || { ui_print "! ThemeManager.smali not found"; return 1; }
+
+  if ! grep -q "removeSmallCurrentstyleOverlays" "$tm"; then
+    method_file="$TMPDIR/remove_dummy_overlay_apks.smali"
+    cat > "$method_file" <<'DUMMY_OVERLAY_METHOD'
+
+.method private removeSmallCurrentstyleOverlays(Ljava/lang/String;)V
+    .registers 12
+
+    :try_start_0
+    iget-object v0, p0, Lcom/samsung/android/thememanager/ThemeManager;->mEnabledPackages:Ljava/util/ArrayList;
+
+    invoke-virtual {v0}, Ljava/util/ArrayList;->iterator()Ljava/util/Iterator;
+
+    move-result-object v0
+
+    :cond_6
+    :goto_6
+    invoke-interface {v0}, Ljava/util/Iterator;->hasNext()Z
+
+    move-result v1
+
+    if-eqz v1, :cond_71
+
+    invoke-interface {v0}, Ljava/util/Iterator;->next()Ljava/lang/Object;
+
+    move-result-object v1
+
+    check-cast v1, Ljava/lang/String;
+
+    new-instance v2, Ljava/lang/StringBuilder;
+
+    invoke-direct {v2}, Ljava/lang/StringBuilder;-><init>()V
+
+    invoke-virtual {v2, v1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    const-string v3, ".apk"
+
+    invoke-virtual {v2, v3}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    invoke-virtual {v2}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
+
+    move-result-object v2
+
+    new-instance v3, Ljava/io/File;
+
+    invoke-direct {v3, p1, v2}, Ljava/io/File;-><init>(Ljava/lang/String;Ljava/lang/String;)V
+
+    invoke-virtual {v3}, Ljava/io/File;->exists()Z
+
+    move-result v2
+
+    if-eqz v2, :cond_6
+
+    invoke-virtual {v3}, Ljava/io/File;->length()J
+
+    move-result-wide v4
+
+    const-wide/16 v6, 0x400
+
+    cmp-long v2, v4, v6
+
+    if-ltz v2, :cond_3e
+
+    goto :goto_6
+
+    :cond_3e
+    const-string v2, "ThemeManager"
+
+    new-instance v4, Ljava/lang/StringBuilder;
+
+    invoke-direct {v4}, Ljava/lang/StringBuilder;-><init>()V
+
+    const-string v5, "skip dummy overlay "
+
+    invoke-virtual {v4, v5}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    invoke-virtual {v4, v1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    invoke-virtual {v4}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
+
+    move-result-object v1
+
+    invoke-static {v2, v1}, Lcom/samsung/android/thememanager/log/Log;->i(Ljava/lang/String;Ljava/lang/String;)I
+
+    invoke-interface {v0}, Ljava/util/Iterator;->remove()V
+
+    invoke-virtual {v3}, Ljava/io/File;->delete()Z
+    :try_end_5e
+    .catch Ljava/lang/Exception; {:try_start_0 .. :try_end_5e} :catch_5f
+
+    goto :goto_6
+
+    :catch_5f
+    move-exception v0
+
+    const-string v1, "ThemeManager"
+
+    const-string v2, "removeSmallCurrentstyleOverlays failed"
+
+    invoke-static {v1, v2}, Lcom/samsung/android/thememanager/log/Log;->e(Ljava/lang/String;Ljava/lang/String;)I
+
+    invoke-virtual {v0}, Ljava/lang/Exception;->printStackTrace()V
+
+    :cond_71
+    return-void
+.end method
+DUMMY_OVERLAY_METHOD
+
+    tmp="$tm.tmp"
+    awk -v add="$method_file" '
+      { print }
+      /^\.method .*doCopy\(Ljava\/lang\/String;Ljava\/io\/InputStream;Ljava\/lang\/String;\)Ljava\/io\/File;/ {
+        in_do_copy = 1
+      }
+      in_do_copy && /^\.end method/ {
+        while ((getline line < add) > 0) print line
+        close(add)
+        in_do_copy = 0
+        added = 1
+      }
+      END {
+        if (!added) exit 1
+      }
+    ' "$tm" > "$tmp" || return 1
+    mv -f "$tmp" "$tm"
+  fi
+
+  if ! grep -q "invoke-direct .*removeSmallCurrentstyleOverlays(Ljava/lang/String;)V" "$tm"; then
+    tmp="$tm.tmp"
+    awk '
+      function emit_dummy_call(owner) {
+        print "    const-string v1, \"/data/overlays/currentstyle\""
+        print ""
+        print "    invoke-direct {" owner ", v1}, Lcom/samsung/android/thememanager/ThemeManager;->removeSmallCurrentstyleOverlays(Ljava/lang/String;)V"
+        print ""
+      }
+      function flush_pending() {
+        if (pending != "") {
+          print pending
+          for (i = 1; i <= buffered; i++) print buffer[i]
+          pending = ""
+          owner = ""
+          buffered = 0
+        }
+      }
+      {
+        if (pending != "") {
+          buffer[++buffered] = $0
+
+          if ($0 ~ /removeSmallCurrentstyleOverlays\(Ljava\/lang\/String;\)V/) {
+            flush_pending()
+            next
+          }
+
+          if ($0 ~ /applyOpenThemeOverlays\(Ljava\/util\/List;Ljava\/util\/List;ILandroid\/content\/om\/ISamsungOverlayCallback;\)V/) {
+            emit_dummy_call(owner)
+            print pending
+            for (i = 1; i <= buffered; i++) print buffer[i]
+            pending = ""
+            owner = ""
+            buffered = 0
+            patched++
+            next
+          }
+
+          if (buffered >= 25) {
+            flush_pending()
+          }
+          next
+        }
+
+        if ($0 ~ /iget-object .*Lcom\/samsung\/android\/thememanager\/ThemeManager;->mOverlayMangaer:Lcom\/samsung\/android\/thememanager\/ThemeManager\$OverlayManager;/) {
+          owner = $0
+          sub(/^.*iget-object [^,]*, /, "", owner)
+          sub(/, Lcom\/samsung\/android\/thememanager\/ThemeManager;->mOverlayMangaer.*/, "", owner)
+          pending = $0
+          buffered = 0
+          next
+        }
+
+        print
+      }
+      END {
+        flush_pending()
+        if (!patched) exit 1
+      }
+    ' "$tm" > "$tmp" || return 1
+    mv -f "$tmp" "$tm"
+  fi
+
+  ui_print "  Patched: ThemeManager.smali :: dummy overlay APK filter"
+}
+
 ##############################################################################
 # 3 ── Module installation
 ##############################################################################
@@ -140,7 +342,7 @@ smali_kit() {
 ui_print "-------------------------------------------------- "
 ui_print " Universal ThemeCenter Patch for Samsung           "
 ui_print "-------------------------------------------------- "
-ui_print " by @BlassGO and @Benj2557     |   Version: 2.0    "
+ui_print " by @BlassGO and @Benj2557     |   Version: 3.0    "
 ui_print "-------------------------------------------------- "
 ui_print " "
 
@@ -184,6 +386,7 @@ ui_print " "
 ui_print " -- Patching smali methods..."
 smali_kit -c -m "setTrialExpiredPackage" -remake "$dummy" -d "$TMPDIR/center"
 smali_kit -c -m "setAlarm"               -remake "$dummy" -d "$TMPDIR/center"
+patch_dummy_overlay_apks "$TMPDIR/center" || abort " ! Dummy APK patch failed"
 ui_print " "
 
 # ── Recompile ─────────────────────────────────────────────────────────────────
